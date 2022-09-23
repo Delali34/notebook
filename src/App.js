f85992637b12f1246278a3a9c23b061230e3fa07
Dela <ernestanyomitse752@gmail.com>
2022-09-23 04:44:03 -1200

    notebook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { useState } from 'react';
+import { Container,Row,Col } from "react-bootstrap";
+import AddNotes from './components/AddNotes';
+import SeeAllNotes from './components/SeeAllNotes';
+import './myStyles.css'
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Container>
+    <Row>
+      <Col className='addNotes'>
+      <AddNotes/>
+      </Col>
+    </Row>
+
+    <Row>
+    <Col>
+     <SeeAllNotes />
+      </Col>
+    </Row>
+   </Container>
+   </>
   );
 }
 
